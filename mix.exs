8a05934a619dc94aac56ee9ea8a653a6bb0c160a
Iuri Madeira <iurimadeira@gmail.com>
2019-05-16 19:43:55 -0300

    Adds mix_test_watch; Bumps Elixir version; Bumps lib version;

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule Recurly.Mixfile do
 
   def project do
     [app: :recurly,
-     version: "0.2.2",
-     elixir: "~> 1.3",
+     version: "0.2.3",
+     elixir: "~> 1.8",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      consolidate_protocols: Mix.env == :prod,
@@ -30,6 +30,7 @@ defmodule Recurly.Mixfile do
       {:ex_doc, "~> 0.11", only: :dev},
       {:credo, "~> 0.4", only: [:dev, :test]},
       {:excoveralls, "~> 0.5", only: :test},
+      {:mix_test_watch, "~> 0.8", only: :dev, runtime: false},
       {:bypass, "~> 0.1", only: :test}
     ]
   end
